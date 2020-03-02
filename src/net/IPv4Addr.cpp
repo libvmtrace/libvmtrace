e6d8c1710cb1401505d6e92284ed574782f6affc
Thomas Dangl <thomas.dangl@protonmail.ch>
2020-03-02 17:39:53 +0100

    Remaining networking is namespaced now

--- src/net/IPv4Addr.cpp
+++ src/net/IPv4Addr.cpp
@@ -1,41 +1,51 @@
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <string.h>
-
 #include <iostream>
+#include <net/IPv4Addr.hpp>
 
-#include "net/IPv4Addr.hpp"
-
-IPv4Addr::IPv4Addr(const struct in_addr* buf) {
-    memcpy(&addr, buf, sizeof(struct in_addr));
-}
-IPv4Addr::IPv4Addr(const IPv4Addr& other ) {
-    memcpy(&addr, &other.addr, sizeof(struct in_addr));
-}
-
-IPv4Addr::operator std::string() const 
+namespace libvmtrace
 {
-    char addr_str[128];
-    char *tmp = inet_ntoa(addr);
-    if (tmp)
-    	strcpy(addr_str, tmp);
-    return std::string(addr_str);
+namespace net
+{
+	IPv4Addr::IPv4Addr(const struct in_addr* buf)
+	{
+		memcpy(&addr, buf, sizeof(struct in_addr));
+	}
+
+	IPv4Addr::IPv4Addr(const IPv4Addr& other)
+	{
+		memcpy(&addr, &other.addr, sizeof(struct in_addr));
+	}
+
+	IPv4Addr::operator std::string() const 
+	{
+		char addr_str[128];
+		char* tmp = inet_ntoa(addr);
+		if (tmp)
+			strcpy(addr_str, tmp);
+		return std::string(addr_str);
+	}
+	
+	std::ostream& operator<<(std::ostream& strm, const IPv4Addr& p)
+	{
+		char addr_str[128];
+		char* tmp = inet_ntoa(p.addr);
+		if (tmp)
+			strcpy(addr_str, tmp);
+
+		return strm << addr_str;
+	}
+
+	bool operator==(const IPv4Addr& lhs, const IPv4Addr& rhs)
+	{
+		if (memcmp((void*)&lhs.addr, (void*)&rhs.addr, sizeof(struct in_addr)) == 0)
+			return true;
+		return false;
+	}
+}
 }
 
-std::ostream& operator<<(std::ostream &strm, const IPv4Addr& p) {
-    char addr_str[128];
-    char *tmp = inet_ntoa(p.addr);
-    if (tmp)
-    	strcpy(addr_str, tmp);
 
-    return strm << addr_str;
-}
-
-bool operator==(const IPv4Addr& lhs, const IPv4Addr& rhs) {
-    if (memcmp((void*)&lhs.addr, (void*)&rhs.addr, sizeof(struct in_addr)) == 0)
-        return true;
-    return false;
-}
-
-// vim: tabstop=4 shiftwidth=4 expandtab 
--- src/net/IPv4Addr.hpp
+++ src/net/IPv4Addr.hpp
@@ -1,3 +1,4 @@
+
 #ifndef IPV4ADDR
 #define IPV4ADDR
 
@@ -8,23 +9,29 @@
 
 #include <iostream>
 
-class IPv4Addr {
+namespace libvmtrace
+{
+namespace net
+{
+class IPv4Addr
+{
 public:
-    IPv4Addr(const IPv4Addr& addr );
-    IPv4Addr(const struct in_addr* buf);
-    
-    operator std::string() const;    
-    friend bool operator==(const IPv4Addr& lhs, const IPv4Addr& rhs);
-    friend std::ostream& operator<<(std::ostream&, const IPv4Addr&);
+	IPv4Addr(const IPv4Addr& addr );
+	IPv4Addr(const struct in_addr* buf);
+
+	operator std::string() const;    
+	friend bool operator==(const IPv4Addr& lhs, const IPv4Addr& rhs);
+	friend std::ostream& operator<<(std::ostream&, const IPv4Addr&);
 
 private:
-    struct in_addr addr;
-    
+	struct in_addr addr;
 };
 
 bool operator==(const IPv4Addr& lhs, const IPv4Addr& rhs);
-std::ostream& operator<<(std::ostream &strm, const IPv4Addr* p);
+std::ostream& operator<<(std::ostream& strm, const IPv4Addr* p);
+}
+}
 
 
 #endif
-// vim: tabstop=4 shiftwidth=4 expandtab 
+
