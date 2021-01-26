ecb45bd229fa547e4c0a8bf3dea8d99bcd00d8b0
Thomas Dangl <thomas.dangl@protonmail.ch>
2021-01-26 12:58:12 +0100

    Fixed fast single step condition for syscalls

--- src/sys/BreakpointMechanism.cpp
+++ src/sys/BreakpointMechanism.cpp
@@ -258,7 +258,7 @@ namespace libvmtrace
 				{
 					event->next_slat_id = bpd->slat_id = event->slat_id;
 					event->slat_id = 0;
-					return VMI_EVENT_RESPONSE_SLAT_ID | (/*(b->second.event && b->second.event->IsFast())*/true ?
+					return VMI_EVENT_RESPONSE_SLAT_ID | ((b->second.event && b->second.event->IsFast()) ?
 							VMI_EVENT_RESPONSE_NEXT_SLAT_ID : // here we don't get the step event.
 							VMI_EVENT_RESPONSE_TOGGLE_SINGLESTEP); // and here we do.
 				}
