9900d045edcdd8a83b03ec0d258e550a923709ff
root <root@s7.sis.fim.uni-passau.de>
2021-09-10 06:22:04 +0000

    surround cache flush for kvmi

--- src/sys/BreakpointMechanism.cpp
+++ src/sys/BreakpointMechanism.cpp
@@ -222,11 +222,12 @@ namespace libvmtrace
 		auto bpd = (BPEventData*) &instance->event_data[event->vcpu_id];
 		memcpy(&bpd->regs, event->x86_regs, sizeof(x86_registers_t));
 		event->interrupt_event.reinject = true;
-		
+#ifdef ENABLE_KVMI		
 		// Slow, but KVM needs this one.
 		vmi_pagecache_flush(vmi);
 		vmi_v2pcache_flush(vmi, ~0ull);
 		vmi_pidcache_flush(vmi);
+#endif
 
 		if (vmi_pagetable_lookup(vmi, event->x86_regs->cr3,
 					event->interrupt_event.gla, &bpd->paddr) == VMI_SUCCESS)
